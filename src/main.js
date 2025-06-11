3ed57b3f0b20031c85d2bb5871ab9f9ff4636a22
Fernando Aguilar <feraguilar6985@gmail.com>
2025-06-10 19:02:55 -0600

    feat: simplify user input handling and remove form structure

--- src/main.js
+++ src/main.js
@@ -1,61 +1,13 @@
-import Swal from 'sweetalert2'
-import './style.css'
+let userName = prompt("Please enter your name:");
+console.log(userName);
 
-document.querySelector('#app').innerHTML = `
-  <div class="container">
-    <h1>Formulario de contacto</h1>
+let favoriteColor = prompt("Please enter your favorite color:");
+console.log(favoriteColor);
 
-    <form id="contact-form" class="contact-form">
+localStorage.setItem('userName', userName)
+localStorage.setItem('favoriteColor', favoriteColor);
 
-      <div class="form-group">
-        <label for="name">Nombre:</label>
-        <input type="text" id="name" name="name">
-      </div>
-
-      <div class="form-group">
-        <label for="email">Correo electrónico:</label>
-        <input type="email" id="email" name="email">
-      </div>
-
-      <div class="form-group">
-        <label for="message">Mensaje:</label>
-        <textarea id="message" name="message" rows="4"></textarea>
-      </div>
-
-      <button type="submit" class="submit-button">Enviar</button>
-
-    </form>
-
-    <div class="event-message" id="form-message">
-      Complete el formulario y haga clic en "Enviar".
-    </div>
-  </div>
-`
-
-const form = document.getElementById('contact-form');
-const formMessage = document.getElementById('form-message');
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault()
-  console.log('Formulario enviado');
-
-  const name = document.getElementById('name').value
-  const email = document.getElementById('email').value
-  const message = document.getElementById('message').value
-
-  if (name.trim() === '' || email.trim() === '' || message.trim() === '') {
-
-    Swal.fire({
-      title: 'Error en el formulario',
-      text: 'Por favor, complete todos los campos.',
-      icon: 'error',
-      confirmButtonText: 'Volver a intentar'
-    })
-
-    return;
-  }
-
-  form.reset();
-
-
-})
+let storedUserName = localStorage.getItem('userName');
+let storedFavoriteColor = localStorage.getItem('favoriteColor');
+console.log(`Stored Name: ${storedUserName}`);
+console.log(`Stored Favorite Color: ${storedFavoriteColor}`);
